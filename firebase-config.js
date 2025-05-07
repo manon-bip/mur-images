f5e241d088b27020cfca619837c1ca831d0d3bb9
manon-bip <180704049+manon-bip@users.noreply.github.com>
2025-05-07 23:05:11 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // ⚠️ À REMPLACER PAR TA CONFIGURATION FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyAAjMLJuUGaO1-w-PfbN0ZAQURZ6z4jGYk",
-  authDomain: "mur-images-b5f8e.firebaseapp.com",
-  databaseURL: "https://mur-images-b5f8e-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "mur-images-b5f8e",
-  storageBucket: "mur-images-b5f8e.appspot.com",
-  messagingSenderId: "81776056932",
-  appId: "1:81776056932:web:aca5c4bca5c5314693dd71"
+  apiKey: "AIzaSyC84gP0sKi9BpM5pOqerJ5AmHFlP2F1zVs",
+  authDomain: "mur-images-bce2f.firebaseapp.com",
+  databaseURL: "https://mur-images-bce2f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mur-images-bce2f",
+  storageBucket: "mur-images-bce2f.firebasestorage.app",
+  messagingSenderId: "115234475888",
+  appId: "1:115234475888:web:38982e4ad0632359ba89b5"
 };
 
 firebase.initializeApp(firebaseConfig);
